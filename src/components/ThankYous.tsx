2fb525b230535a371a0ee93b050471f4e74efd2c
Spencer Chang <spencerc99@gmail.com>
2025-03-24 15:27:34 -0700

    add base for thank you

--- src/components/ThankYous.tsx
+++ src/components/ThankYous.tsx
@@ -1,3 +1,22 @@
 export function ThankYous() {
-  return <div>Thank You</div>;
+  return (
+    <div className="flex flex-col gap-8 ">
+      <h1 className="font-bold text-8xl">Thank you notes</h1>
+      <hr />
+      <textarea
+        className="w-full h-full border border-white p-2 min-h-44 bg-transparent"
+        placeholder="Thank you for..."
+      />
+      <div className="flex flex-row gap-4 w-full">
+        <input
+          type="text"
+          placeholder="From"
+          className="bg-transparent border border-white px-4 py-2 w-full"
+        />
+        <button className="bg-yellow-100 text-black px-4 py-2 w-full ">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
